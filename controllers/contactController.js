cafe3df8a96b04307c83ad28d0b6be778560ea20
Dhilipkumar13 <927621bcs026@mkce.ac.in>
2025-01-15 23:54:05 +0530

    learning about how to perform CRUD operation using mongoodb

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,30 +1,68 @@
 const asyncHandler = require("express-async-handler")
+const Contacts = require("../modules/contactModule")
 
 const getContacts = asyncHandler(  async (req,res) =>{
-    res.status(200).json({message: "get all contact"})
+    const contact = await Contacts.find()
+    res.status(200).json(contact)
 })
 
 const createContact = asyncHandler( async (req,res) =>{
-    console.log("Body of contact",req.body);
-    const {title,subject,mark} = res.body
-    if(!title || !subject || !mark){
+    const {name,email,phone} = req.body
+    // console.log("single",name,phone,email)
+    // console.log("Body of contact",req.body);
+
+    if(!name || !email || !phone){
         res.status(400)
         throw new Error("All fields are mandatory !")
     }
+    const contact = await Contacts.create({
+        name,
+        email,
+        phone
+    }) 
 
-    res.status(201).json({message:"create contact"})
+    res.status(201).json(contact)
 })
 
 const getContact = asyncHandler( async (req,res) =>{
-    res.status(200).json({message:`get specific contact ${req.params.id}`})
+
+    const contact = await Contacts.findById(req.params.id)
+  //  console.log("get contact",req.params.id,contact)
+    if(!contact)
+    {
+        res.status(404);
+        throw new Error("Not Found")
+    }
+    res.status(200).json(contact)
 })
 
 const updateContact = asyncHandler( async (req,res) =>{
-    res.status(200).json({message:`update contacts id ${req.params.id}`})
+    const contact = await Contacts.findById(req.params.id)
+  //  console.log("get contact",req.params.id,contact)
+    if(!contact)
+    {
+        res.status(404);
+        throw new Error("Not Found")
+    }
+    const updatedContact = await Contacts.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        {new  : true}
+    )
+    res.status(200).json(updatedContact)
 })
 
 const deleteContact = asyncHandler( async (req,res) =>{
-    res.status(200).json({message:`delete contact id ${req.params.id}`})
+    const contact = await Contacts.findById(req.params.id)
+  //  console.log("get contact",req.params.id,contact)
+    if(!contact)
+    {
+        res.status(404);
+        throw new Error("Not Found")
+    }
+
+    await contact.deleteOne()
+    res.status(200).json(contact)
 })
 
 module.exports = {getContacts,createContact,getContact,updateContact,deleteContact};
--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const connectDb = require("./config/dbConnection")
+const errorHandle = require("./middleware/errorHandling");
 
 const app = express();
 const PORT = process.env.PORT || 5002;
 
+connectDb()
+
 app.use(express.json())
 app.use("/api/contacts", require("./routes/contactRoutes"))
+app.use(errorHandle)
 
 app.get("/", (req, res) => {
     res.send("Hello, World!");
